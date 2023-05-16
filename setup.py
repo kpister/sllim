af119799f382a3461492200f71fe094d5afcdb7b
Kaiser Pister <pisterk@gmail.com>
2023-05-16 08:16:00 -0500

    version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with codecs.open(
 
 setup(
     name="sllim",
-    version="0.1.0",
+    version="0.1.1",
     author="Kaiser Pister",
     author_email="kaiser@pister.dev",
     packages=["sllim"],
